3827979a159150c1ee2bb098d2dfd609ea300043
EnterBrain <vetalbog@gmail.com>
2015-09-12 21:10:46 +0500

    Fix Script

--- ShadowGovernment.user.js
+++ ShadowGovernment.user.js
@@ -11,18 +11,5 @@
 // @require        http://code.jquery.com/jquery.js
 // ==/UserScript==
 
-var script = document.createElement( "script" );
-script.type = "text/javascript";
-script.src = "https://rawgit.com/EnterBrain/Shadow-Government/master/ShadowGovernmentLib.js";
-document.body.appendChild( script );
-script = undefined;
-
-var style = document.createElement( "link" );
-style.type = "text/css";
-style.href = "https://rawgit.com/EnterBrain/Shadow-Government/master/ShadowGovernmentStyle.css";
-style.rel = "stylesheet";
-document.body.appendChild( style );
-style = undefined;
-
-/* $('<script src="https://rawgit.com/EnterBrain/Shadow-Government/master/ShadowGovernmentLib.js" type="text/javascript"></script>').appendTo($("body"));
-$('<link href="https://rawgit.com/EnterBrain/Shadow-Government/master/ShadowGovernmentStyle.css" type="text/css" rel="stylesheet">').appendTo($("body")); */
+$('<script src="https://rawgit.com/EnterBrain/Shadow-Government/master/ShadowGovernmentLib.js" type="text/javascript"></script>').appendTo($("body"));
+$('<link href="https://rawgit.com/EnterBrain/Shadow-Government/master/ShadowGovernmentStyle.css" type="text/css" rel="stylesheet">').appendTo($("body"));
