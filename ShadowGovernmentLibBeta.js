2fd71345e9db3ef7da4475ff75b364e381c9d27e
EnterBrain <vetalbog@gmail.com>
2015-09-18 21:52:12 +0500

    Hot fix 2

--- ShadowGovernmentLibBeta.js
+++ ShadowGovernmentLibBeta.js
@@ -1096,13 +1096,11 @@ $(document).ready(function () {
 				var pageId = ($("#pagination-digg > li.next").length==1) ? parseInt($("#pagination-digg > li.next").prev("li").children("a").html()) : parseInt($("#pagination-digg > li.next-off").prev("li").html());
 				if (id == undefined) { id = 1; }
 				if (id <= parseInt(pageId)){
-					$.ajax({
-						url: "/militaryUnitDonations.html?page="+id,
-					})
+					$.ajax({url: "/militaryUnitDonations.html?page="+id,})
 					.done(function( data, textStatus, jqXHR ) {
 						$(jqXHR.responseText).find("#userMenu + div table.dataTable.paddedTable tr:not(:first)").insertAfter("#userMenu + script + div table.dataTable.paddedTable tr:last");
 						id++;
-						getPageMUDonations(id,pageId);
+						getPageMUDonations(id);
 					});
 				}
 			};
@@ -1116,27 +1114,21 @@ $(document).ready(function () {
 	
 	function TransactionLog(){
 		if ($("#pagination-digg")){
-			var LogUrl = /transactionLog\.html\?type=[\w_]+&dayFrom=\d+&dayTo=\d+/.exec(localUrl);
-			var lastPage = parseInt(($("#pagination-digg > li.next").length==1) ? $("#pagination-digg > li.next").prev("li").children("a").html() : $("#pagination-digg > li.next-off").prev("li").html());
-			var Id = 1;
-			
-			function getPageMUDonations(){
-				if (Id <= lastPage){
-					$.ajax({
-						url: "/"+LogUrl[0]+Id,
-					})
+			function getPageMUDonations(id){
+				var LogUrl = /transactionLog\.html\?type=[\w_]+&dayFrom=\d+&dayTo=\d+/.exec(localUrl);
+				var lastPage = parseInt(($("#pagination-digg > li.next").length==1) ? $("#pagination-digg > li.next").prev("li").children("a").html() : $("#pagination-digg > li.next-off").prev("li").html());
+				if (id <= lastPage){
+					$.ajax({url: "/"+LogUrl[0]+id,})
 					.done(function( data, textStatus, jqXHR ) {
 						$(jqXHR.responseText).find("#userMenu + div table.dataTable.paddedTable tr:not(:first)").insertAfter("#userMenu + script + div table.dataTable.paddedTable tr:last");
-						Id++;
-						getPageMUDonations();
+						id++;
+						getPageMUDonations(id);
 					});
 				}
 			};
-			
-			
 			$('<li class="FullLog">Full Log</li>').appendTo("#pagination-digg").click(function(){
 				$("#userMenu + script + div table.dataTable.paddedTable tr:not(:first)").remove();
-				getPageMUDonations();
+				getPageMUDonations(1);
 			});
 		}
 	}
