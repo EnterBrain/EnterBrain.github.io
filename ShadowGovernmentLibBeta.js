3308e1e89c1622e1134df835d325e6425ebaa9de
EnterBrain <vetalbog@gmail.com>
2015-09-23 10:47:57 +0500

    Hot Fix 3

--- ShadowGovernmentLibBeta.js
+++ ShadowGovernmentLibBeta.js
@@ -825,15 +825,16 @@ function LangByCC( CC ) {
 
 function ImgSrcFix(){
 	$("img").each(function(){
-		/* if ($(this).attr("src")){ */
+		if ($(this).attr("src") != undefined){
 			if ($(this).attr("src").indexOf( "//cdn.e-sim.org//", 0 ) >= 0){
 				$(this).attr("src", $(this).attr("src").replace("//cdn.e-sim.org//","//cdn.e-sim.org/"));
 			}
-		/* } */
-		
-		/* if ($(this).attr("href").indexOf( "https://cdn.e-sim.org:8080/", 0 ) >= 0){
-			$(this).attr("href", $(this).attr("href").replace("https://cdn.e-sim.org:8080/","http://cdn.e-sim.org/"));
-		} */
+		}
+		if ($(this).attr("href") != undefined){
+			if ($(this).attr("href").indexOf( "https://cdn.e-sim.org:8080/", 0 ) >= 0){
+				$(this).attr("href", $(this).attr("href").replace("https://cdn.e-sim.org:8080/","http://cdn.e-sim.org/"));
+			}
+		}
 	});
 }
 /*---Small core function---*/
