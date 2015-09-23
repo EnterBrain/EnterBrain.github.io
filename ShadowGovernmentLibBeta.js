b55f4fddf3c2202bbf7ab46008554f56cc5e0199
EnterBrain <vetalbog@gmail.com>
2015-09-23 11:01:21 +0500

    Hot Fix 6

--- ShadowGovernmentLibBeta.js
+++ ShadowGovernmentLibBeta.js
@@ -824,19 +824,21 @@ function LangByCC( CC ) {
 }
 
 function ImgSrcFix(){
-	$("img").load(function(){
-		if ($(this).attr("src") != undefined){
-			if ($(this).attr("src").indexOf( "//cdn.e-sim.org//", 0 ) >= 0){
-				$(this).attr("src", $(this).attr("src").replace("//cdn.e-sim.org//","//cdn.e-sim.org/"));
+	$(window).load(function () {
+		$("img").each(function(){
+			if ($(this).attr("src") != undefined){
+				if ($(this).attr("src").indexOf( "//cdn.e-sim.org//", 0 ) >= 0){
+					$(this).attr("src", $(this).attr("src").replace("//cdn.e-sim.org//","//cdn.e-sim.org/"));
+				}
 			}
-		}
-	});
-	$("image").load(function(){
-		if ($(this).attr("href") != undefined){
-			if ($(this).attr("href").indexOf( "https://cdn.e-sim.org:8080/", 0 ) >= 0){
-				$(this).attr("href", $(this).attr("href").replace("https://cdn.e-sim.org:8080/","http://cdn.e-sim.org/"));
+		});
+		$("image").each(function(){
+			if ($(this).attr("href") != undefined){
+				if ($(this).attr("href").indexOf( "https://cdn.e-sim.org:8080/", 0 ) >= 0){
+					$(this).attr("href", $(this).attr("href").replace("https://cdn.e-sim.org:8080/","http://cdn.e-sim.org/"));
+				}
 			}
-		}
+		});
 	});
 }
 /*---Small core function---*/
