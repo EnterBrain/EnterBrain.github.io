db63401144dcfe186ef793fa80e1e367898f9b2b
EnterBrain <vetalbog@gmail.com>
2015-09-23 08:26:41 +0500

    Hot Fix IMG SRC v3

--- ShadowGovernmentLibBeta.js
+++ ShadowGovernmentLibBeta.js
@@ -829,7 +829,7 @@ function ImgSrcFix(){
 			$(this).attr("src", $(this).attr("src").replace("//cdn.e-sim.org//","//cdn.e-sim.org/"));
 		}
 		if ($(this).attr("href").indexOf( "https://cdn.e-sim.org:8080/", 0 ) >= 0){
-			$(this).attr("href", $(this).attr("href").replace("https://cdn.e-sim.org:8080/","https://cdn.e-sim.org/"));
+			$(this).attr("href", $(this).attr("href").replace("https://cdn.e-sim.org:8080/","http://cdn.e-sim.org/"));
 		}
 	});
 }
