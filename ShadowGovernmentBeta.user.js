eac29fd6be2636e432e83326ba4f1fc7ee027e53
EnterBrain <vetalbog@gmail.com>
2016-01-16 22:17:36 +0500

    Hot fix user.js

--- ShadowGovernmentBeta.user.js
+++ ShadowGovernmentBeta.user.js
@@ -4,13 +4,13 @@
 // @namespace      localhost
 // @author         EnterBrain
 // @description    Plugin for best experience Shadow Government.
-// @icon           http://enterbrain.github.io/img/16.png
-// @icon64         http://enterbrain.github.io/img/64.png
-// @match          http://*.e-sim.org/*
+// @icon           https://enterbrain.github.io/img/16.png
+// @icon64         https://enterbrain.github.io/img/64.png
+// @match          http*://*.e-sim.org/*
 // @grant          all
-// @require        http://code.jquery.com/jquery.js
+// @require        https://code.jquery.com/jquery.js
 // ==/UserScript==
 
-$('<script src="http://enterbrain.github.io/plugins/jquery.blockUI.js" type="text/javascript"></script>').appendTo($("head"));
-$('<script src="http://enterbrain.github.io/ShadowGovernmentLibBeta.js" type="text/javascript"></script>').appendTo($("head"));
-$('<link href="http://enterbrain.github.io/css/ShadowGovernmentStyleBeta.css" type="text/css" rel="stylesheet">').appendTo($("head"));
+$('<script src="https://enterbrain.github.io/plugins/jquery.blockUI.js" type="text/javascript"></script>').appendTo($("head"));
+$('<script src="https://enterbrain.github.io/ShadowGovernmentLibBeta.js" type="text/javascript"></script>').appendTo($("head"));
+$('<link href="https://enterbrain.github.io/css/ShadowGovernmentStyleBeta.css" type="text/css" rel="stylesheet">').appendTo($("head"));
