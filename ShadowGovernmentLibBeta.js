12f89472663c37023a8b19648b22694bf39fc373
EnterBrain <vetalbog@gmail.com>
2016-03-31 00:10:19 +0500

    Notify fix v2

--- ShadowGovernmentLibBeta.js
+++ ShadowGovernmentLibBeta.js
@@ -2886,7 +2886,7 @@ function BattleStatsMinimize(){
 	$("#battleStats").show();
 				
 	$("#battleSelectable:first + #battleSelectable div.small-10:first > #moreBattleStats").remove();
-	$('#battleSelectable:first + #battleSelectable div.small-10:first').insertAfter( $('#battleStats') );
+	$('#battleStats').insertBefore( $('#battleSelectable:first + #battleSelectable div.small-10:first') );
 	$('<div id="wrapperBattleStatsButtons" class="foundation-style small-10 columns" style="margin-bottom:.4em;"></div>').insertBefore($("#battleSelectable:first + #battleSelectable div.small-4:first"));
 	$('<div id="showTop3BattleStats" style="padding-bottom:.4em;padding-top:.4em;margin:.4em .4em 0 .4em;" class="foundation-style button"> Show Top 3 </div>').appendTo($("#wrapperBattleStatsButtons"));
 	$('<div id="showTop10BattleStats" style="padding-bottom:.4em;padding-top:.4em;margin:.4em .4em 0 .4em;" class="foundation-style button"> Show Top 10 </div>').appendTo($("#wrapperBattleStatsButtons"));
