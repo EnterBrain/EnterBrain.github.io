745fa2f409e3507ab3f0c2d994c3d14222d6ac97
EnterBrain <vetalbog@gmail.com>
2016-09-12 14:44:38 +0500

    Hot fix version

--- ShadowGovernmentBeta.user.js
+++ ShadowGovernmentBeta.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Shadow Government Extension Beta
-// @version        1.31
+// @version        1.311
 // @namespace      localhost
 // @author         EnterBrain
 // @description    Plugin for best experience Shadow Government.
