f2f28520a4484db49bafec371c3f65394f5a7144
EnterBrain <vetalbog@gmail.com>
2016-09-12 14:42:49 +0500

    tablesorter in beta

--- ShadowGovernmentBeta.user.js
+++ ShadowGovernmentBeta.user.js
@@ -11,6 +11,11 @@
 // @require        https://code.jquery.com/jquery.js
 // ==/UserScript==
 
+$('<script src="https://enterbrain.github.io/plugins/jquery.metadata.js" type="text/javascript"></script>').appendTo($("head"));
 $('<script src="https://enterbrain.github.io/plugins/jquery.blockUI.js" type="text/javascript"></script>').appendTo($("head"));
+$('<script src="https://enterbrain.github.io/plugins/jquery.tablesorter.js" type="text/javascript"></script>').appendTo($("head"));
+$('<script src="https://enterbrain.github.io/plugins/jquery.tablesorter.pager.js" type="text/javascript"></script>').appendTo($("head"));
+$('<link href="https://enterbrain.github.io/css/jquery.tablesorter.style.css" type="text/css" rel="stylesheet">').appendTo($("head"));
+$('<link href="https://enterbrain.github.io/css/jquery.tablesorter.pager.css" type="text/css" rel="stylesheet">').appendTo($("head"));
 $('<script src="https://enterbrain.github.io/ShadowGovernmentLibBeta.js" type="text/javascript"></script>').appendTo($("head"));
 $('<link href="https://enterbrain.github.io/css/ShadowGovernmentStyleBeta.css" type="text/css" rel="stylesheet">').appendTo($("head"));
