042921657f82aef8428c5a9a306b7d25afb30dd7
EnterBrain <vetalbog@gmail.com>
2016-09-16 13:58:33 +0500

    SG 2.0 release

--- ShadowGovernment.user.js
+++ ShadowGovernment.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Shadow Government Extension
-// @version        1.4
+// @version        2.0
 // @namespace      localhost
 // @author         EnterBrain
 // @description    Plugin for best experience Shadow Government.
@@ -11,12 +11,12 @@
 // @require        https://code.jquery.com/jquery.js
 // ==/UserScript==
 
-$('<script src="https://enterbrain.github.io/plugins/jquery.metadata.js" type="text/javascript"></script>').appendTo($("head"));
-$('<script src="https://enterbrain.github.io/plugins/jquery.blockUI.js" type="text/javascript"></script>').appendTo($("head"));
-$('<script src="https://enterbrain.github.io/plugins/jquery.tablesorter.min.js" type="text/javascript"></script>').appendTo($("head"));
-$('<script src="https://enterbrain.github.io/plugins/jquery.tablesorter.pager.js" type="text/javascript"></script>').appendTo($("head"));
+$('<script defer src="https://enterbrain.github.io/plugins/jquery.metadata.js" type="text/javascript"></script>').appendTo($("head"));
+$('<script defer src="https://enterbrain.github.io/plugins/jquery.blockUI.js" type="text/javascript"></script>').appendTo($("head"));
+$('<script defer src="https://enterbrain.github.io/plugins/jquery.tablesorter.min.js" type="text/javascript"></script>').appendTo($("head"));
+$('<script defer src="https://enterbrain.github.io/plugins/jquery.tablesorter.pager.js" type="text/javascript"></script>').appendTo($("head"));
 $('<link href="https://enterbrain.github.io/css/jquery.tablesorter.style.css" type="text/css" rel="stylesheet">').appendTo($("head"));
 $('<link href="https://enterbrain.github.io/css/jquery.tablesorter.pager.css" type="text/css" rel="stylesheet">').appendTo($("head"));
-$('<script src="https://enterbrain.github.io/ShadowGovernmentDB.min.js" type="text/javascript"></script>').appendTo($("head"));
-$('<script src="https://enterbrain.github.io/ShadowGovernmentLib.min.js" type="text/javascript"></script>').appendTo($("head"));
+$('<script defer src="https://enterbrain.github.io/ShadowGovernmentDB.min.js" type="text/javascript"></script>').appendTo($("head"));
+$('<script defer src="https://enterbrain.github.io/ShadowGovernmentLib.min.js" type="text/javascript"></script>').appendTo($("head"));
 $('<link href="https://enterbrain.github.io/css/ShadowGovernmentStyle.css" type="text/css" rel="stylesheet">').appendTo($("head"));
--- ShadowGovernmentBeta.user.js
+++ ShadowGovernmentBeta.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Shadow Government Extension Beta
-// @version        1.4
+// @version        2.0
 // @namespace      localhost
 // @author         EnterBrain
 // @description    Plugin for best experience Shadow Government.
@@ -11,12 +11,12 @@
 // @require        https://code.jquery.com/jquery.js
 // ==/UserScript==
 
-$('<script src="https://enterbrain.github.io/plugins/jquery.metadata.js" type="text/javascript"></script>').appendTo($("head"));
-$('<script src="https://enterbrain.github.io/plugins/jquery.blockUI.js" type="text/javascript"></script>').appendTo($("head"));
-$('<script src="https://enterbrain.github.io/plugins/jquery.tablesorter.js" type="text/javascript"></script>').appendTo($("head"));
-$('<script src="https://enterbrain.github.io/plugins/jquery.tablesorter.pager.js" type="text/javascript"></script>').appendTo($("head"));
+$('<script defer src="https://enterbrain.github.io/plugins/jquery.metadata.js" type="text/javascript"></script>').appendTo($("head"));
+$('<script defer src="https://enterbrain.github.io/plugins/jquery.blockUI.js" type="text/javascript"></script>').appendTo($("head"));
+$('<script defer src="https://enterbrain.github.io/plugins/jquery.tablesorter.js" type="text/javascript"></script>').appendTo($("head"));
+$('<script defer src="https://enterbrain.github.io/plugins/jquery.tablesorter.pager.js" type="text/javascript"></script>').appendTo($("head"));
 $('<link href="https://enterbrain.github.io/css/jquery.tablesorter.style.css" type="text/css" rel="stylesheet">').appendTo($("head"));
 $('<link href="https://enterbrain.github.io/css/jquery.tablesorter.pager.css" type="text/css" rel="stylesheet">').appendTo($("head"));
-$('<script src="https://enterbrain.github.io/ShadowGovernmentDB.js" type="text/javascript"></script>').appendTo($("head"));
-$('<script src="https://enterbrain.github.io/ShadowGovernmentLibBeta.js" type="text/javascript"></script>').appendTo($("head"));
+$('<script defer src="https://enterbrain.github.io/ShadowGovernmentDB.js" type="text/javascript"></script>').appendTo($("head"));
+$('<script defer src="https://enterbrain.github.io/ShadowGovernmentLibBeta.js" type="text/javascript"></script>').appendTo($("head"));
 $('<link href="https://enterbrain.github.io/css/ShadowGovernmentStyleBeta.css" type="text/css" rel="stylesheet">').appendTo($("head"));
