f1948161ac745a473bee79b65a28abe716066736
EnterBrain <vetalbog@gmail.com>
2017-10-29 07:13:25 +0500

    ProgressWrap fix on PM

--- ShadowGovernmentLibBeta.js
+++ ShadowGovernmentLibBeta.js
@@ -2186,7 +2186,7 @@ function createTablePM(){
 		lastPageId = lastPageRaw[2];
 	}
 
-	$('<hr class="littleDashedLine"><div id="productProgressWrap"><center><p style="text-align: center;"><img alt="" src="'+IMGLOAD+'" style="margin-right: 10px;" /><span style="font-size:36px;"><span id="CountPage">0</span>/<span id="AllPage">'+lastPageId+'</span>&nbsp;Pages loaded</span></p></center></div><p style="clear: both"></p>').appendTo(".small-8 > .testDivblue");
+	$('<hr class="littleDashedLine"><div id="productProgressWrap"><center><p style="text-align: center;"><img alt="" src="'+IMGLOAD+'" style="margin-right: 10px;" /><span style="font-size:36px;"><span id="CountPage">0</span>/<span id="AllPage">'+lastPageId+'</span>&nbsp;Pages loaded</span></p></center></div><p style="clear: both"></p>').appendTo(".small-8 > .testDivwhite");
 
 	var pagerHTML = '<div id="pager" class="pager"><form><img src="'+IMGFIRSTBTN+'" class="first"/><img src="'+IMGPREVBTN+'" class="prev"/><input type="text" class="pagedisplay"/><img src="'+IMGNEXTBTN+'" class="next"/><img src="'+IMGLASTBTN+'" class="last"/><select class="pagesize"><option selected="selected"  value="10">10</option><option value="20">20</option><option value="30">30</option><option  value="40">40</option></select></form></div>';
 	$(".dataTable tr:first td").each(function(){arrTest[arrTest.length] = $(this).text()});
@@ -2385,7 +2385,7 @@ function CalcValuePM(){
 	console.log(taxesPush);
 
 	$('<div id="currencyProgressWrap"><center><p style="text-align: center;"><img alt="" src="'+IMGLOAD+'" style="margin-right: 10px;" /><span style="font-size:36px;"><span id="CountCurrency">0</span>/<span id="AllCurrency">'+Object.keys(currencyHash).length+'</span>&nbsp;Currencies loaded</span></p></center></div><p style="clear: both"></p>').appendTo(".small-8 > .testDivwhite");
-	//$('<div id="taxesProgressWrap"><center><p style="text-align: center;"><img alt="" src="'+IMGLOAD+'" style="margin-right: 10px;" /><span style="font-size:36px;"><span id="CountTax">0</span>/<span id="AllTax">'+Object.keys(taxesHash).length+'</span>&nbsp;Taxes loaded</span></p></center></div><p style="clear: both"></p>').appendTo(".small-8 > .testDivblue");
+	//$('<div id="taxesProgressWrap"><center><p style="text-align: center;"><img alt="" src="'+IMGLOAD+'" style="margin-right: 10px;" /><span style="font-size:36px;"><span id="CountTax">0</span>/<span id="AllTax">'+Object.keys(taxesHash).length+'</span>&nbsp;Taxes loaded</span></p></center></div><p style="clear: both"></p>').appendTo(".small-8 > .testDivwhite");
 
 	function currencyHashAdd(){
 		// setTimeout( function() {
